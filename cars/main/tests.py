42d454e38905f8b2595c780a11317b543d6128f1
Victoris <52378018+sviridchik@users.noreply.github.com>
2021-08-17 14:54:47 +0300

    added 8.2

--- cars/main/tests.py
+++ cars/main/tests.py
@@ -1181,3 +1181,62 @@ class Car_delete_test_pk(APITestCase):
         response = self.client.delete('/cars/{}'.format(id))
         self.assertEqual(response.status_code, 401)
 #
+# ========== viewed cars free ===========
+
+
+class Car_free_get_test(APITestCase):
+    payload_car = {
+        "level_consumption": 2,
+        "mark": "Mercedes-Benz",
+        "reg_number": "MP31523",
+        "color": "w",
+        "year": 2000,
+        "latitude": 55.0,
+        "status": "active",
+        "car_class": 1,
+        "longitude": 37.0}
+
+    def setUp(self):
+        self.user = self.client.post('/users/signup',
+                                     data={'username': 'test123', 'password': 'test123123', "dtp_times": 9,
+                                           "email": "test@gmail.com", "date_of_birth": "2003-09-09"})
+
+        response = self.client.post('/users/signin', data={'username': 'test123', 'password': 'test123123'})
+        self.token = response.data['access']
+        payload = {
+            "price_for_km": 23,
+            "night_add": 13,
+            "price_dtp": 8,
+            "parking_price": 9,
+            "booking_price": 23,
+            "description": "very informative"
+        }
+
+        self.api_authentication()
+        Profile.objects.all().update(is_admin=True)
+        response = self.client.post('/price/', data=payload)
+
+        response = self.client.post('/class/', data={
+            "id": 1,
+            "name": "comfort",
+            "price": 1,
+            "booking_time": 15
+        })
+        response = self.client.post('/cars/', data=Car_get_test.payload_car)
+
+        Profile.objects.all().update(is_admin=False)
+
+    def api_authentication(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
+
+    def test_cars(self):
+        self.api_authentication()
+        Profile.objects.all().update(is_admin=True)
+
+        response = self.client.get('/cars/free/?latitude=55&longitude=37&distance=100&class=economy&ordering=distance')
+        self.assertEqual(response.status_code, 200)
+
+    def test_cars_not_admin(self):
+        self.api_authentication()
+        response = self.client.get('/cars/free/?latitude=55&longitude=37&distance=100&class=economy&ordering=distance')
+        self.assertEqual(response.status_code, 200)
