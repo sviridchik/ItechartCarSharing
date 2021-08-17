ec75317174c1d95ab171feb50f0bf985cfb6ab5e
Victoris <52378018+sviridchik@users.noreply.github.com>
2021-08-17 14:40:35 +0300

    added 8.2

--- cars/main/serializer.py
+++ cars/main/serializer.py
@@ -73,3 +73,13 @@ class CarSerializer(serializers.ModelSerializer):
         model = Cars
         fields = ('id','level_consumption', 'mark', 'reg_number','color','year','latitude','status','car_class','longitude')
         read_only_fields = ('id',)
+
+
+class ViewedCarSerializer(serializers.ModelSerializer):
+    id = serializers.IntegerField(read_only = True)
+
+
+    class Meta:
+        model = ViewedCars
+        fields = ('id','car', 'price_day','price_night','user','booking_price')
+        read_only_fields = ('id',)
