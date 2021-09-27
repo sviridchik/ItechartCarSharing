ca9cdc9485272c4bf687c7bce493c727d9d3d32e
VictoriaS <viccisviri@gmail.com>
2021-09-27 09:39:07 +0300

    fixaed docker

--- api/cars/settings.py
+++ api/cars/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
         'NAME': 'postgres',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': 5432,
     }
 }
