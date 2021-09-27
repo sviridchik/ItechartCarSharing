070319c0e581b91414e57f88c895b93e7cde4026
VictoriaS <viccisviri@gmail.com>
2021-09-27 09:51:04 +0300

    corrections

--- api/price/models.py
+++ api/price/models.py
@@ -3,9 +3,9 @@ from main.models import BaseModel
 # Create your models here.
 class Price(BaseModel):
     # id = models.IntegerField(primary_key=True, auto_created=True, verbose_name='id')
-    price_for_km = models.FloatField(blank=True)
-    night_add = models.FloatField(blank=True)
-    price_dtp = models.FloatField(blank=True)
-    parking_price = models.FloatField(blank=True)
-    booking_price = models.FloatField(blank=True)
+    price_for_km = models.FloatField(null=False, blank=False)
+    night_add = models.FloatField(null=False, blank=False)
+    price_dtp = models.FloatField(null=False, blank=False)
+    parking_price = models.FloatField(null=False, blank=False)
+    booking_price = models.FloatField(null=False, blank=False)
     description = models.TextField()
--- api/price/permissions.py
+++ api/price/permissions.py
@@ -2,15 +2,10 @@ from rest_framework.permissions import BasePermission
 
 from users.models import Profile
 
-
 class MyPermissionAdminNotUser(BasePermission):
     def has_permission(self, request, view):
         if request.method == 'GET':
             return True
         user = request.user
         user_profile = Profile.objects.get(user=user)
-
-        if not user_profile.is_admin:
-            return False
-        else:
-            return True
+        return user_profile.is_admin
